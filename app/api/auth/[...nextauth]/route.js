980d2faa266cf9810ac5579bcbae3f341e060958
Harshal Raikwar <harshalraikwar07@gmail.com>
2023-06-13 19:41:17 +0530

    ✨ Update auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,6 @@ import prisma from '../../../libs/prismadb'
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
 import bcrypt from 'bcrypt'
 
 export const authOptions = {
@@ -21,35 +20,25 @@ export const authOptions = {
                 username: { label: "Username", type: "text", placeholder: "John Smith" },
             },
             async authorize(credentials) {
-              
-                // check to see if email and password is there
-                if(!credentials.email || !credentials.password) {
+                if (!credentials.email || !credentials.password) {
                     throw new Error('Please enter an email and password')
                 }
-
-                // check to see if user exists
                 const user = await prisma.user.findUnique({
                     where: {
                         email: credentials.email
                     }
                 });
-
-                // if no user was found 
                 if (!user || !user?.hashedPassword) {
                     throw new Error('No user found')
                 }
-
-                // check to see if password matches
                 const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
-
-                // if password does not match
                 if (!passwordMatch) {
                     throw new Error('Incorrect password')
                 }
 
                 return user;
             },
-        }),  
+        }),
     ],
     secret: process.env.SECRET,
     session: {
@@ -59,4 +48,4 @@ export const authOptions = {
 }
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
