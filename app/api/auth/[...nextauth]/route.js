1b9212acbda57cf2fced22c13fac5fc285c24866
Harshal Raikwar <harshalraikwar07@gmail.com>
2023-06-13 18:50:30 +0530

    ✨ Update deploy auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,62 +1,16 @@
-import NextAuth from "next-auth/next";
-import prisma from '../../../libs/prismadb'
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
-import bcrypt from 'bcrypt'
 
-export const authOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                email: { label: "Email", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" },
-                username: { label: "Username", type: "text", placeholder: "John Smith" },
-            },
-            async authorize(credentials) {
-              
-                // check to see if email and password is there
-                if(!credentials.email || !credentials.password) {
-                    throw new Error('Please enter an email and password')
-                }
-
-                // check to see if user exists
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                });
-
-                // if no user was found 
-                if (!user || !user?.hashedPassword) {
-                    throw new Error('No user found')
-                }
-
-                // check to see if password matches
-                const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
-
-                // if password does not match
-                if (!passwordMatch) {
-                    throw new Error('Incorrect password')
-                }
-
-                return user;
-            },
-        }),  
-    ],
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
     secret: process.env.SECRET,
-    session: {
-        strategy: "jwt",
-    },
-    debug: process.env.NODE_ENV === "development",
-}
+  },
+});
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
